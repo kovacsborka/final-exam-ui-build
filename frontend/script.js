17d2264ac0ec56137b4de6ec758141e32262fd80
kovacsborka <kovacsborbalaterez@gmail.com>
2022-05-18 12:37:02 +0200

    almost done

---
+++ frontend/script.js
@@ -0,0 +1,116 @@
+const books = {
+	id: "books",
+	logo: "New York Times Best Sellers",
+	button: "read more",
+	cards: [
+		{
+			number: 1,
+			title: "Breakfast of Champions",
+			sub: "Kurt Vonnegut",
+			text: "In Breakfast of Champions, one of Kurt Vonnegut`s most beloved characters, the aging writer Kilgore Trout, finds to his horror that a Midwest car dealer is taking his fiction as truth. What follows is murderously funny satire, as Vonnegut looks at war, sex, racism, success, politics, and..."
+		},
+		{
+			number: 2,
+			title: "Known Unknowns",
+			sub: "Charles Saatchi",
+			text: "In his latest book, Known Unknowns, Charles Saatchi provides fascinating insights into some of the world`s lesser-known but truly extraordinary historical events. Illustrated with arresting photographs, Saatchi`s 100 essays tackle subjects as varied as the tattoo habits of Russian criminals... perspective on contemporary culture."
+		},
+		{
+			number: 3,
+			title: "Empire V",
+			sub: "Victor Pelevin",
+			text: "Roman thought he`d found the perfect opportunity to rebel. He may have been wrong.	He awakens strapped to a set of parallel bars in a richly appointed sitting room, and begins a conversation with a masked man which will change his life. His world has been a facade - one which the ..."
+		},
+		{
+			number: 4,
+			title: "The Master and Margarita",
+			sub: "Mikhail Bulgakov",
+			text: "An audacious revision of the stories of Faust and Pontius Pilate, The Master and Margarita is recognized as one of the essential classics of modern Russian literature. The novel's vision of Soviet life in the 1930s is so ferociously accurate that it could not be published during its..."
+		},
+		{
+			number: 5,
+			title: "The Map and the Territory",
+			sub: "Michel Houellebecq",
+			text: "Having made his name with an exhibition of photographs of Michelin roadmaps - beautiful works that won praise from every corner of the art world - Jed Martin is now emerging from a ten-year hiatus. And he has had some good news. It has nothing to do with his broken boiler, the approach..."
+		},
+		{
+			number: 6,
+			title: "The Neverending Story",
+			sub: "Michael Ende",
+			text: "This epic work of the imagination has captured the hearts of millions of readers worldwide since it was first published. Its special story within a story is an irresistible invitation for readers to become part of the book itself... "
+		},
+		{
+			number: 7,
+			title: "To the End of the Land",
+			sub: "David Grossman",
+			text: "From one of Israel`s most acclaimed writers comes a novel of extraordinary power about family life—the greatest human drama—and the cost of war.Ora, a middle-aged Israeli mother, is on the verge of celebrating her son Ofer`s release from army service when he returns to the front for a major..."
+		},
+		{
+			number: 8,
+			title: "One Hundred Years of Solitude",
+			sub: "Gabriel García Márquez",
+			text: "The brilliant, bestselling, landmark novel that tells the story of the Buendia family, and chronicles the irreconcilable conflict between the desire for solitude and the need for love—in rich, imaginative prose that has come to define an entire genre known as magical realism..."
+		},
+		{
+			number: 9,
+			title: "Beautiful World, Where Are You",
+			sub: "Sally Rooney",
+			text: "Alice, a novelist, meets Felix, who works in a warehouse, and asks him if he`d like to travel to Rome with her. In Dublin, her best friend, Eileen, is getting over a break-up and slips back into flirting with Simon, a man she has known since childhood. Alice, Felix, Eileen, and..."
+		},
+		{
+			number: 10,
+			title: "Rendezvous with Rama",
+			sub: "Arthur C. Clarke",
+			text: "At first, only a few things are known about the celestial object that astronomers dub Rama. It is huge, weighing more than ten trillion tons. And it is hurtling through the solar system at an inconceivable speed. Then a space probe confirms the unthinkable: Rama is no natural object..."
+		}
+	]
+}
+console.log(books);
+
+
+function loadEvent() {
+
+
+
+    let rootElement = document.getElementById("root")
+
+
+	let header = `
+		<header class="header">
+			<h1 class="title">New York Times Bestsellers</h1>
+			<button class="material-icons">
+			menu
+			</button>
+		</header>
+	`
+
+	rootElement.insertAdjacentHTML("beforeend", header)
+
+
+	let cardsElement = document.getElementById("cards")
+	
+
+    books.cards.forEach(card => {
+		
+        let htmlC = `
+			<div class="card">
+				<div class=card-infos>
+					<h3>${card.number}</h3>
+					<h4>${card.sub}</h4>
+					<h2>${card.title}</h2>
+					<p>${card.text}</p>
+				</div>	
+				<div class="buttons">
+					<button class="readmore"  >read more</button>
+					<button  class="material-icons">arrow_forward</button>
+				</div>
+			</div>
+		
+		`;
+        cardsElement.insertAdjacentHTML("beforeend", htmlC)
+    });
+
+    
+
+}
+window.addEventListener("load", loadEvent)
